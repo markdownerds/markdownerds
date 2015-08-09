81562ccb5e233fadf9bec1b7e96a87e9f98ab9ca
Joost Ouwerling <joost.ouwerling@mobiyou.nl>
2015-08-08 23:48:22 -0700

    User gets doc after login

---
+++ client/templates/login/login.js
@@ -0,0 +1,16 @@
+Accounts.onLogin(function() {
+
+  if(Session.get("docId")) {
+    console.log("The session has a doc id")
+    doc = Documents.findOne(Session.get("docId"));
+    if(doc && doc.owner == null) {
+      console.log("the doc exists and the owner is null, so update.")
+      Documents.update(doc._id, {
+        $set: {
+          owner: Meteor.userId()
+        }
+      });
+    }
+  }
+  
+});
