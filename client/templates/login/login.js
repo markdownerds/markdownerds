e91f3f748053449873fdc33c5d2f5272e222a625
dhfromkorea <dhllee424@gmail.com>
2015-08-09 08:13:11 -0700

    custom login form implemented

--- client/templates/login/login.js
+++ client/templates/login/login.js
@@ -1,21 +1,51 @@
-Accounts.onLogin(function() {
+;(function(Template) {
 
-  if(Session.get("docId")) {
-    console.log("The session has a doc id")
-    doc = Documents.findOne(Session.get("docId"));
-    if(doc && doc.owner == null) {
-      console.log("the doc exists and the owner is null, so update.")
-      Documents.update(doc._id, {
-        $set: {
-          owner: Meteor.userId()
-        }
-      }, function() {
-        Router.go('workpane', {_id: doc._id});
-      });
+  var onLogin = function() {
+    if (Session.get("docId")) {
+      console.log("The session has a doc id");
+      doc = Documents.findOne(Session.get("docId"));
+      if (doc && !doc.owner) {
+        console.log("the doc exists and the owner is null, so update.");
+        Documents.update(doc._id, {
+          $set: {
+            owner: Meteor.userId()
+          }
+        }, function() {
+          Router.go('workpane', {
+            _id: doc._id
+          });
+        });
+      }
+
     }
-    
-  }
+    Router.go('/');
+  };
 
-  Router.go('/');
+  Template.login.events({
+    'submit #login-form': function(e, t) {
+      e.preventDefault();
+      // retrieve the input field values
+      var email = t.find('#login-email').value,
+        password = t.find('#login-password').value;
 
-});
+      // Trim and validate your fields here....
+
+      // If validation passes, supply the appropriate fields to the
+      // Meteor.loginWithPassword() function.
+      Meteor.loginWithPassword(email, password, function(err) {
+        if (err) {
+          console.log('opps login failed');
+        }
+        // The user might not have been found, or their passwword
+        // could be incorrect. Inform the user that their
+        // login attempt has failed.
+        else {
+          onLogin();
+        }
+        // The user has been logged in.
+      });
+      return false;
+    }
+  });
+
+})(Template);
