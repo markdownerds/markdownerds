3216002923c479fc7d5d4f94c36827adb0d0ed51
dhfromkorea <dhllee424@gmail.com>
2015-08-09 08:46:35 -0700

    login implemented

--- client/templates/login/login.js
+++ client/templates/login/login.js
@@ -24,25 +24,14 @@
   Template.login.events({
     'submit #login-form': function(e, t) {
       e.preventDefault();
-      // retrieve the input field values
       var email = t.find('#login-email').value,
         password = t.find('#login-password').value;
-
-      // Trim and validate your fields here....
-
-      // If validation passes, supply the appropriate fields to the
-      // Meteor.loginWithPassword() function.
       Meteor.loginWithPassword(email, password, function(err) {
         if (err) {
           console.log('opps login failed');
-        }
-        // The user might not have been found, or their passwword
-        // could be incorrect. Inform the user that their
-        // login attempt has failed.
-        else {
+        }else {
           onLogin();
         }
-        // The user has been logged in.
       });
       return false;
     }
--- client/templates/signup/singup.js
+++ client/templates/signup/singup.js
@@ -38,7 +38,7 @@
   // };
 
   Template.signup.events({
-    'submit #register-form': function(e, t) {
+    'submit #signup-form': function(e, t) {
       e.preventDefault();
       var email = t.find('#account-email').value,
         password = t.find('#account-password').value;
